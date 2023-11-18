88cd57d457950225152dee874194f88a58327e82
AnandBhalla <150023512+AnandBhalla@users.noreply.github.com>
2023-11-18 19:25:11 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const products=[
     {
         id:1,
-        single_product:'product1.html',
+        single_product:'sproduct1.html',
         product_image:'images/shirts/product1.png',
         product_company:'LEE COOPER',
         product_name:'shirt',
@@ -11,7 +11,7 @@ const products=[
     },
     {
         id:2,
-        single_product:'product1.html',
+        single_product:'sproduct2.html',
         product_image:'images/shirts/product2.png',
         product_company:'RED TAPE',
         product_name:'shirt',
@@ -20,7 +20,7 @@ const products=[
     },
     {
         id:3,
-        single_product:'product1.html',
+        single_product:'sproduct3.html',
         product_image:'images/shirts/product3.png',
         product_company:'BEING HUMAN',
         product_name:'shirt',
@@ -29,7 +29,7 @@ const products=[
     },
     {
         id:20,
-        single_product:'product1.html',
+        single_product:'sproduct4.html',
         product_image:'images/shirts/product5.png',
         product_company:'ARMANI',
         product_name:'shirt',
@@ -38,7 +38,7 @@ const products=[
     },
     {
         id:4,
-        single_product:'product1.html',
+        single_product:'sproduct5.html',
         product_image:'images/exclusive/ex1.avif',
         product_company:'SNITCH',
         product_name:'shirt',
@@ -47,7 +47,7 @@ const products=[
     },
     {
         id:5,
-        single_product:'product1.html',
+        single_product:'sproduct6.html',
         product_image:'images/exclusive/ex2.avif',
         product_company:'SNITCH',
         product_name:'shirt',
@@ -56,7 +56,7 @@ const products=[
     },
     {
         id:6,
-        single_product:'product1.html',
+        single_product:'sproduct7.html',
         product_image:'images/exclusive/ex3.jpg',
         product_company:'SNITCH',
         product_name:'shirt',
@@ -65,7 +65,7 @@ const products=[
     },
     {
         id:7,
-        single_product:'product1.html',
+        single_product:'sproduct8.html',
         product_image:'images/exclusive/ex4.avif',
         product_company:'SNITCH',
         product_name:'shirt',
@@ -74,7 +74,7 @@ const products=[
     },
     {
         id:8,
-        single_product:'product1.html',
+        single_product:'sproduct9.html',
         product_image:'images/jeans/jean1.jpeg',
         product_company:'LEE COOPER',
         product_name:'jeans',
@@ -83,7 +83,7 @@ const products=[
     },
     {
         id:9,
-        single_product:'product1.html',
+        single_product:'sproduct10.html',
         product_image:'images/jeans/jean2.avif',
         product_company:'RED TAPE',
         product_name:'jeans',
@@ -93,7 +93,7 @@ const products=[
     
     {
         id:10,
-        single_product:'product1.html',
+        single_product:'sproduct11.html',
         product_image:'images/jeans/jean3.jpeg',
         product_company:'VAN HEUSEN',
         product_name:'jeans',
@@ -102,7 +102,7 @@ const products=[
     },
     {
         id:11,
-        single_product:'product1.html',
+        single_product:'sproduct12.html',
         product_image:'images/jeans/jean4.avif',
         product_company:'LEVIS',
         product_name:'jeans',
@@ -111,7 +111,7 @@ const products=[
     },
     {
         id:12,
-        single_product:'product1.html',
+        single_product:'sproduct13.html',
         product_image:'images/winter/product1.jpg',
         product_company:'LEE COOPER',
         product_name:'hoodie',
@@ -120,7 +120,7 @@ const products=[
     },
     {
         id:13,
-        single_product:'product1.html',
+        single_product:'sproduct14.html',
         product_image:'images/winter/product2.webp',
         product_company:'RED TAPE',
         product_name:'hoodie',
@@ -129,7 +129,7 @@ const products=[
     },
     {
         id:14,
-        single_product:'product1.html',
+        single_product:'sproduct15.html',
         product_image:'images/winter/product3.jpg',
         product_company:'ROADSTER',
         product_name:'hoodie',
@@ -139,7 +139,7 @@ const products=[
     
     {
         id:15,
-        single_product:'product1.html',
+        single_product:'sproduct16.html',
         product_image:'images/winter/product4.png',
         product_company:'LEVIS',
         product_name:'hoodie',
@@ -148,7 +148,7 @@ const products=[
     },
     {
         id:16,
-        single_product:'product1.html',
+        single_product:'sproduct17.html',
         product_image:'images/diwali/product1.avif',
         product_company:'ALLEN SOLLY',
         product_name:'jacket',
@@ -157,7 +157,7 @@ const products=[
     },
     {
         id:17,
-        single_product:'product1.html',
+        single_product:'sproduct18.html',
         product_image:'images/diwali/product2.avif',
         product_company:'LEE COOPER',
         product_name:'jacket',
@@ -166,7 +166,7 @@ const products=[
     },
     {
         id:18,
-        single_product:'product1.html',
+        single_product:'sproduct19.html',
         product_image:'images/diwali/product3.avif',
         product_company:'RED TAPE',
         product_name:'jacket',
@@ -175,7 +175,7 @@ const products=[
     },
     {
         id:19,
-        single_product:'product1.html',
+        single_product:'sproduct20.html',
         product_image:'images/diwali/product4.avif',
         product_company:'WROGN',
         product_name:'jacket',
